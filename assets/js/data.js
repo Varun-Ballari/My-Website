b318e7b25e85d0c2c547113edb80dac80d61127e
Varun Ballari <varun@ballari.com>
2016-10-22 17:15:47 -0400

    javascript added

---
+++ assets/js/data.js
@@ -0,0 +1,32 @@
+var projects = [
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood is the sobriquet for the Hindi language film industry based in Mumbai, India.", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"},
+    {"pic":"assets/images/mypic.png", "projName":"ATLAS", "projDetails":"Bollywood Fusion", "facebookLink": "https://www.facebook.com/"}
+];
+
+
+var codeIcons = [
+    {"pic":"assets/images/icons/xcode.png", "software":"Xcode"},
+    {"pic":"assets/images/icons/swift.png", "software":"Swift 3"},
+    {"pic":"assets/images/icons/java.png", "software":"Java 8"},
+    {"pic":"assets/images/icons/python.png", "software":"Python"},
+    {"pic":"assets/images/icons/flask.png", "software":"Python Flask"},
+    {"pic":"assets/images/icons/html.png", "software":"HTML 5"},
+    {"pic":"assets/images/icons/css.png", "software":"CSS 3"},
+    {"pic":"assets/images/icons/js.png", "software":"Javascript"},
+    {"pic":"assets/images/icons/node.png", "software":"Node JS"},
+    {"pic":"assets/images/icons/mysql.png", "software":"MySql"},
+    {"pic":"assets/images/icons/postgres.png", "software":"Postgres"},
+    {"pic":"assets/images/icons/heroku.png", "software":"Heroku"}
+
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,42 @@
-var offset = $("#first").offset();
-$("#heading").css("margin-left", offset.left);
-$("#heading").css("margin-right", offset.left);
-$("#links").css("margin-left", offset.left);
-$("#links").css("margin-right", offset.left);
+function getProjects(arr) {
+    var out = "";
+    for(var i = 0; i < arr.length; i++) {
+        if (i == 0) {
+            out += '<div class="project_class" id="first">'
+        } else {
+            out += '<div class="project_class">'
+        }
+        out += `
+                    <div class="project_image" <!--style="background-image: url(` + arr[i].pic + `)"--> >
+                        <h1>1</h1>
+                    </div>
+                    <div class="project_info">
+                        <h1>` + arr[i].projName + `</h1>
+                        <h2>` + arr[i].projDetails + `</h2>
+                    </div>
+                    <div class="project_links">
+                        <div class="github">
+                            <h1>link</h1>
+                        </div>
+                    </div>
+                </div>`;
+    }
+    document.getElementById("projects").innerHTML = out;
+}
 
-$(window).on('resize', function(){
-    var offset = $("#first").offset();
-    $("#heading").css("margin-left", offset.left);
-    $("#heading").css("margin-right", offset.left);
-    $("#links").css("margin-left", offset.left);
-    $("#links").css("margin-right", offset.left);
-});
+function getCodeIcons(arr) {
+    var out = "";
+    for(var i = 0; i < arr.length; i++) {
+        out += `<div class="codeIcon_class">
+                    <div class="codeIcon_image" style="background-image: url(` + arr[i].pic + `)">
+                    </div>
+                    <div class="codeIcon_info">
+                        <h1>` + arr[i].software + `</h1>
+                    </div>
+                </div>`;
+    }
+    document.getElementById("codeIcons").innerHTML = out;
+}
+
+getProjects(projects);
+getCodeIcons(codeIcons);
